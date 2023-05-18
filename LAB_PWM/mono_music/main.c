08d280fa7c45111afd52604f8438d975cfca9f64
HieuHoangWork <hieuhoang.bkhn.dev@gmail.com>
2023-05-18 11:29:04 +0700

    Update

--- LAB_PWM/mono_music/main.c
+++ LAB_PWM/mono_music/main.c
@@ -14,124 +14,34 @@
 #define PWM_DISABLE 0
 #define PWM_ENABLE 1
 
-/*************************************************
- * Public Constants pitches.h
- *************************************************/
-#define NOTE_B0 31
-#define NOTE_C1 33
-#define NOTE_CS1 35
-#define NOTE_D1 37
-#define NOTE_DS1 39
-#define NOTE_E1 41
-#define NOTE_F1 44
-#define NOTE_FS1 46
-#define NOTE_G1 49
-#define NOTE_GS1 52
-#define NOTE_A1 55
-#define NOTE_AS1 58
-#define NOTE_B1 62
-#define NOTE_C2 65
-#define NOTE_CS2 69
-#define NOTE_D2 73
-#define NOTE_DS2 78
-#define NOTE_E2 82
-#define NOTE_F2 87
-#define NOTE_FS2 93
-#define NOTE_G2 98
-#define NOTE_GS2 104
-#define NOTE_A2 110
-#define NOTE_AS2 117
-#define NOTE_B2 123
-#define NOTE_C3 131
-#define NOTE_CS3 139
-#define NOTE_D3 147
-#define NOTE_DS3 156
-#define NOTE_E3 165
-#define NOTE_F3 175
-#define NOTE_FS3 185
-#define NOTE_G3 196
-#define NOTE_GS3 208
-#define NOTE_A3 220
-#define NOTE_AS3 233
-#define NOTE_B3 247
-#define NOTE_C4 262
-#define NOTE_CS4 277
-#define NOTE_D4 294
-#define NOTE_DS4 311
-#define NOTE_E4 330
-#define NOTE_F4 349
-#define NOTE_FS4 370
-#define NOTE_G4 392
-#define NOTE_GS4 415
-#define NOTE_A4 440
-#define NOTE_AS4 466
-#define NOTE_B4 494
-#define NOTE_C5 523
-#define NOTE_CS5 554
-#define NOTE_D5 587
-#define NOTE_DS5 622
-#define NOTE_E5 659
-#define NOTE_F5 698
-#define NOTE_FS5 740
-#define NOTE_G5 784
-#define NOTE_GS5 831
-#define NOTE_A5 880
-#define NOTE_AS5 932
-#define NOTE_B5 988
-#define NOTE_C6 1047
-#define NOTE_CS6 1109
-#define NOTE_D6 1175
-#define NOTE_DS6 1245
-#define NOTE_E6 1319
-#define NOTE_F6 1397
-#define NOTE_FS6 1480
-#define NOTE_G6 1568
-#define NOTE_GS6 1661
-#define NOTE_A6 1760
-#define NOTE_AS6 1865
-#define NOTE_B6 1976
-#define NOTE_C7 2093
-#define NOTE_CS7 2217
-#define NOTE_D7 2349
-#define NOTE_DS7 2489
-#define NOTE_E7 2637
-#define NOTE_F7 2794
-#define NOTE_FS7 2960
-#define NOTE_G7 3136
-#define NOTE_GS7 3322
-#define NOTE_A7 3520
-#define NOTE_AS7 3729
-#define NOTE_B7 3951
-#define NOTE_C8 4186
-#define NOTE_CS8 4435
-#define NOTE_D8 4699
-#define NOTE_DS8 4978
+// TONES
+#define C 2100
+#define D 1870
+#define E 1670
+#define f 1580 // Does not seem to like capital F
+#define G 1400
+// Define a special note, 'R', to represent a rest
+#define R 0
 
 // Định nghĩa các nốt nhạc cho bài hát
-int melody[] = {
-    NOTE_E4, NOTE_E4, NOTE_E4,
-    NOTE_E4, NOTE_E4, NOTE_E4,
-    NOTE_E4, NOTE_G4, NOTE_C4, NOTE_D4, NOTE_E4,
-    NOTE_F4, NOTE_F4,
-    NOTE_E4, NOTE_D4, NOTE_D4, NOTE_E4, NOTE_D4, NOTE_G4,
-    NOTE_E4, NOTE_E4, NOTE_E4,
-    NOTE_E4, NOTE_E4, NOTE_E4,
-    NOTE_E4, NOTE_G4, NOTE_C4, NOTE_D4, NOTE_E4,
-    NOTE_F4, NOTE_F4,
-    NOTE_E4, NOTE_D4, NOTE_D4, NOTE_E4, NOTE_D4, NOTE_G4};
+int melody[] = {E, E, E, R,
+                E, E, E, R,
+                E, G, C, D, E, R,
+                f, f, f, f, f, E, E, E, E, D, D, E, D, R, G, R,
+                E, E, E, R,
+                E, E, E, R,
+                E, G, C, D, E, R,
+                f, f, f, f, f, E, E, E, G, G, f, D, C, R};
 
 // Định nghĩa thời lượng cho mỗi nốt nhạc
-int noteDurations[] = {
-    8, 8, 4,
-    8, 8, 4,
-    8, 8, 8, 8, 2,
-    8, 8,
-    8, 8, 8, 8, 8, 2,
-    8, 8, 4,
-    8, 8, 4,
-    8, 8, 8, 8, 2,
-    8, 8,
-    8, 8, 8, 8, 8, 2};
+int noteDurations[] = {4, 4, 4, 4,
+                       4, 4, 4, 4,
+                       4, 4, 4, 4, 4, 4,
+                       4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+                       4, 4, 4, 4,
+                       4, 4, 4, 4,
+                       4, 4, 4, 4, 4, 4,
+                       4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
 
 void sigint_handler(int sig_num);
 void PWM_controller(int pwm_channel, char *path_controller);
@@ -150,12 +60,18 @@ int main()
         int note = melody[i];
         int note_duration = noteDurations[i];
 
-        // Đặt tần số (period) của PWM để phù hợp với nốt nhạc
-        set_PWM_period(1000000 / note, PWM0_PATH); // 1s/Hz = us
-        set_PWM_duty(500000 / note, PWM0_PATH);    // 50% duty cycle
-
-        // Enable PWM để chơi nốt nhạc
-        PWM_status(PWM_ENABLE, PWM0_PATH);
+        if (note != R)
+        {                                                 // Only perform division if note is not a rest
+            set_PWM_period(1000000000 / note, PWM0_PATH); // 1s/Hz = us
+            set_PWM_duty(500000000 / note, PWM0_PATH);    // 50% duty cycle
+            // Enable PWM to play the note
+            PWM_status(PWM_ENABLE, PWM0_PATH);
+        }
+        else
+        {
+            // Rest (note == R), so disable PWM
+            PWM_status(PWM_DISABLE, PWM0_PATH);
+        }
 
         // Đợi cho đến khi nốt nhạc kết thúc
         usleep(note_duration * 125000); // 125 ms cho mỗi đơn vị thời gian
@@ -164,7 +80,7 @@ int main()
         PWM_status(PWM_DISABLE, PWM0_PATH);
 
         // Đợi một chút trước khi chơi nốt nhạc tiếp theo
-        usleep(30000); // Đợi 30 ms
+        usleep(1000); // Đợi 1 ms
     }
 
     // Tắt PWM khi kết thúc
